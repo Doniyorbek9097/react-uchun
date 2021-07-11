71b6276c03d79b09a0f5183ea6dc6bca595ea310
Doniyorbek9097 <doniyorbek3322@mail.ru>
2021-07-11 05:53:01 +0100

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Main from "./containers/Main";
 export default function App() {
   return (
     <div className="container">
-      
       <Main/>
     </div>
   );
--- src/conponents/Home.js
+++ src/conponents/Home.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React from 'react';
 
-export default function Home(){
+export default function Home() {
   return (
     <div className="Home">
-      <h1>home</h1>
+      <div className="about-me-box">
+        <h1>Hey, I am DONIYORBEK ERGASHEV</h1>
+        <p>
+          - a web developer based in Namangan, Uzbekistan. In 2017, after
+          <br/>
+          <br/>
+          graduated from Tashkent University of Information Technologies, I
+          started my career as a front-end web developer. Over the last 3 years
+          I have been building digital products and helping people bring their
+          ideas come to life. I ,as a developer, love coding, learning a new
+          technology, reading a book and having a coffee in the morning :).
+        </p>
+        <p>
+          Enough about me. Let’s make the world a better place. If you have any
+          cool projects or opportunities, get in touch! 
+        </p>
+
+        <div className="contact-box">
+          <a href="tel:+99834073322">CONTACT</a>
+        </div>
+      </div>
+      <div className="my-img-box">
+        <img
+          src="https://stackblitz.com/files/react-uchun/github/Doniyorbek9097/react-uchun/master/png-transparent-zac-efron-high-school-musical-makin-the-cut-celebrity-troy-bolton-zac-efron-2015.png"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
